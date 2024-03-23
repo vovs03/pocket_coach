4f51bf1f9d09548d7e3f88b15272d9ed2773b69c
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2024-03-23 23:38:02 +0300

    Add: Questions 210-239; little refactoring(pre_cleaning)

--- lib/features/random_question/random_quesion_logic.dart
+++ lib/features/random_question/random_quesion_logic.dart
@@ -12,8 +12,20 @@ class RandomStringGenerator extends StatefulWidget {
 
 class _RandomStringGeneratorState extends State<RandomStringGenerator> {
   // 'Hello', 'Flutter', 'World', 'Dart', 
-  List<String> strings = [q200, q201, q202, q203,q204, 
-                          q205, q206, q207, q208, q209];
+  
+  List<String> strings =[q200, q201, q202, q203, q204, q205, q206, q207, q208, q209,
+                         q210, q211, q212, q213, q214, q215, q216, q217, q218, q219,
+                         q220, q221, q222, q223, q224, q225, q226, q227, q228, q229,
+                         q230, q231, q232, q233, q234, q235, q236, q237, q238, q239 
+  ];
+
+  // List<String> ten200 =[q200, q201, q202, q203,q204, q205, q206, q207, q208, q209];
+  // List<String> heckto200 = [...ten200,];
+  // List<String> strings = [...heckto200,];
+
+  // var ten200 = ten200 =[q200, q201, q202, q203,q204, q205, q206, q207, q208, q209];
+  // var strings = hecto200;
+
   String currentString = 'Приступим к разбору вопросов.';
 
   void updateText() {
